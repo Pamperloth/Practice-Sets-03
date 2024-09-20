a73813b76f232b78bb40e8ef0ac18547641dd6c8
Pamperloth <125177508+Pamperloth@users.noreply.github.com>
2024-09-20 23:41:40 +0800

    Add files via upload

---
+++ Practice-Sets-03/PS03_01/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter 1st Side: ");
+        double a =Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter 2nd Side: ");
+        double b=Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter 3rd Side: ");
+        double c=Convert.ToInt32(Console.ReadLine());
+
+        double Area=Herons_Formula(a,b,c);
+        
+        Console.WriteLine("The Area of this Triangle is: " + Area);
+    }
+    static double Herons_Formula(double a, double b, double c)
+    {
+        double s = (a + b + c)/2;
+        double Area = Math.Sqrt(s*(s-a)*(s-b)*(s-c));
+        return Area;
+    }
+}
---
+++ Practice-Sets-03/PS03_02/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter Year: ");
+        int year=Convert.ToInt32(Console.ReadLine());
+
+        if (Leap_Test(year))
+        {
+            Console.WriteLine("Leap year.");
+        }
+        else
+        {
+            Console.WriteLine("Not a leap year.");
+        }
+    }
+
+    static bool Leap_Test (int year)
+    {
+        return (year % 4 == 0 && year % 100 != 0) || (year % 400 == 0);
+    }
+}
